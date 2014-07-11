723123e52e7d4aa1511dd6c8af92ee6f814899f7
matakmonika <mmatak@foi.hr>
2014-07-11 23:52:37 +0200

    promjenapregledrn
    promijenjeni dizajn pregleda radnih naloga

--- AplikacijaPI nije dobro/AplikacijaPI/FrmPregledRadnihNaloga.Designer.cs
+++ AplikacijaPI nije dobro/AplikacijaPI/FrmPregledRadnihNaloga.Designer.cs
@@ -34,18 +34,17 @@
             this.lblVozilo = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.korisnikBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.t16_DBDataSet = new AplikacijaPI.T16_DBDataSet();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.t16_DBDataSet = new AplikacijaPI.T16_DBDataSet();
-            this.korisnikBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.voziloBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.korisnikTableAdapter = new AplikacijaPI.T16_DBDataSetTableAdapters.KorisnikTableAdapter();
             this.tableAdapterManager = new AplikacijaPI.T16_DBDataSetTableAdapters.TableAdapterManager();
-            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.voziloBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.voziloTableAdapter = new AplikacijaPI.T16_DBDataSetTableAdapters.VoziloTableAdapter();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.radni_nalogBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.radni_nalogTableAdapter = new AplikacijaPI.T16_DBDataSetTableAdapters.Radni_nalogTableAdapter();
+            this.voziloTableAdapter = new AplikacijaPI.T16_DBDataSetTableAdapters.VoziloTableAdapter();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.iDradninalogDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.sifrazaposlenikaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.datumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -53,45 +52,52 @@
             this.iDkorisnikaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.statusDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.popravakDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.t16_DBDataSet)).BeginInit();
+            this.radni_nalogBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.korisnikBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t16_DBDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.voziloBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radni_nalogBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblRadnikS
             // 
             this.lblRadnikS.AutoSize = true;
-            this.lblRadnikS.Location = new System.Drawing.Point(26, 48);
+            this.lblRadnikS.Location = new System.Drawing.Point(24, 29);
+            this.lblRadnikS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRadnikS.Name = "lblRadnikS";
-            this.lblRadnikS.Size = new System.Drawing.Size(73, 13);
+            this.lblRadnikS.Size = new System.Drawing.Size(95, 17);
             this.lblRadnikS.TabIndex = 0;
             this.lblRadnikS.Text = "Radnik Servis";
             // 
             // lblKlijent
             // 
             this.lblKlijent.AutoSize = true;
-            this.lblKlijent.Location = new System.Drawing.Point(30, 85);
+            this.lblKlijent.Location = new System.Drawing.Point(29, 75);
+            this.lblKlijent.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblKlijent.Name = "lblKlijent";
-            this.lblKlijent.Size = new System.Drawing.Size(35, 13);
+            this.lblKlijent.Size = new System.Drawing.Size(46, 17);
             this.lblKlijent.TabIndex = 1;
             this.lblKlijent.Text = "Klijent";
             // 
             // lblVozilo
             // 
             this.lblVozilo.AutoSize = true;
-            this.lblVozilo.Location = new System.Drawing.Point(26, 135);
+            this.lblVozilo.Location = new System.Drawing.Point(24, 123);
+            this.lblVozilo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblVozilo.Name = "lblVozilo";
-            this.lblVozilo.Size = new System.Drawing.Size(35, 13);
+            this.lblVozilo.Size = new System.Drawing.Size(46, 17);
             this.lblVozilo.TabIndex = 2;
             this.lblVozilo.Text = "Vozilo";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 435);
+            this.button1.Location = new System.Drawing.Point(27, 457);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 28);
+            this.button1.Size = new System.Drawing.Size(151, 34);
             this.button1.TabIndex = 6;
             this.button1.Text = "Ažururaj";
             this.button1.UseVisualStyleBackColor = true;
@@ -103,20 +109,32 @@
             this.comboBox1.DataSource = this.korisnikBindingSource;
             this.comboBox1.DisplayMember = "sifra_zaposlenika";
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(112, 48);
+            this.comboBox1.Location = new System.Drawing.Point(138, 29);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 7;
             this.comboBox1.ValueMember = "sifra_zaposlenika";
+            // 
+            // korisnikBindingSource
+            // 
+            this.korisnikBindingSource.DataMember = "Korisnik";
+            this.korisnikBindingSource.DataSource = this.t16_DBDataSet;
+            // 
+            // t16_DBDataSet
+            // 
+            this.t16_DBDataSet.DataSetName = "T16_DBDataSet";
+            this.t16_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // comboBox2
             // 
             this.comboBox2.DataSource = this.korisnikBindingSource;
             this.comboBox2.DisplayMember = "ime";
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(110, 86);
+            this.comboBox2.Location = new System.Drawing.Point(136, 76);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(122, 21);
+            this.comboBox2.Size = new System.Drawing.Size(161, 24);
             this.comboBox2.TabIndex = 8;
             this.comboBox2.ValueMember = "ime";
             // 
@@ -126,21 +144,17 @@
             this.comboBox3.DataSource = this.voziloBindingSource;
             this.comboBox3.DisplayMember = "registracijska_oznaka";
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(110, 132);
+            this.comboBox3.Location = new System.Drawing.Point(136, 119);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(122, 21);
+            this.comboBox3.Size = new System.Drawing.Size(161, 24);
             this.comboBox3.TabIndex = 9;
             this.comboBox3.ValueMember = "registracijska_oznaka";
             // 
-            // t16_DBDataSet
+            // voziloBindingSource
             // 
-            this.t16_DBDataSet.DataSetName = "T16_DBDataSet";
-            this.t16_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // korisnikBindingSource
-            // 
-            this.korisnikBindingSource.DataMember = "Korisnik";
-            this.korisnikBindingSource.DataSource = this.t16_DBDataSet;
+            this.voziloBindingSource.DataMember = "Vozilo";
+            this.voziloBindingSource.DataSource = this.t16_DBDataSet;
             // 
             // korisnikTableAdapter
             // 
@@ -158,19 +172,18 @@
             this.tableAdapterManager.UpdateOrder = AplikacijaPI.T16_DBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             this.tableAdapterManager.VoziloTableAdapter = this.voziloTableAdapter;
             // 
-            // notifyIcon1
+            // radni_nalogTableAdapter
             // 
-            this.notifyIcon1.Text = "notifyIcon1";
-            this.notifyIcon1.Visible = true;
-            // 
-            // voziloBindingSource
-            // 
-            this.voziloBindingSource.DataMember = "Vozilo";
-            this.voziloBindingSource.DataSource = this.t16_DBDataSet;
+            this.radni_nalogTableAdapter.ClearBeforeFill = true;
             // 
             // voziloTableAdapter
             // 
             this.voziloTableAdapter.ClearBeforeFill = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
             // 
             // dataGridView1
             // 
@@ -185,19 +198,11 @@
             this.statusDataGridViewTextBoxColumn,
             this.popravakDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.radni_nalogBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 198);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 178);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(741, 201);
+            this.dataGridView1.Size = new System.Drawing.Size(743, 254);
             this.dataGridView1.TabIndex = 10;
-            // 
-            // radni_nalogBindingSource
-            // 
-            this.radni_nalogBindingSource.DataMember = "Radni_nalog";
-            this.radni_nalogBindingSource.DataSource = this.t16_DBDataSet;
-            // 
-            // radni_nalogTableAdapter
-            // 
-            this.radni_nalogTableAdapter.ClearBeforeFill = true;
             // 
             // iDradninalogDataGridViewTextBoxColumn
             // 
@@ -242,11 +247,25 @@
             this.popravakDataGridViewTextBoxColumn.HeaderText = "popravak";
             this.popravakDataGridViewTextBoxColumn.Name = "popravakDataGridViewTextBoxColumn";
             // 
+            // radni_nalogBindingSource
+            // 
+            this.radni_nalogBindingSource.DataMember = "Radni_nalog";
+            this.radni_nalogBindingSource.DataSource = this.t16_DBDataSet;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AplikacijaPI.Properties.Resources.laguna;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(886, 518);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
             // FrmPregledRadnihNaloga
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(765, 475);
+            this.ClientSize = new System.Drawing.Size(878, 513);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
@@ -255,14 +274,17 @@
             this.Controls.Add(this.lblVozilo);
             this.Controls.Add(this.lblKlijent);
             this.Controls.Add(this.lblRadnikS);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmPregledRadnihNaloga";
             this.Text = "FrmPregledRadnihNaloga";
             this.Load += new System.EventHandler(this.FrmPregledRadnihNaloga_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.t16_DBDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.korisnikBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t16_DBDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.voziloBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radni_nalogBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -294,5 +316,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn iDkorisnikaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn statusDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn popravakDataGridViewTextBoxColumn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- AplikacijaPI nije dobro/AplikacijaPI/Properties/Resources.Designer.cs
+++ AplikacijaPI nije dobro/AplikacijaPI/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace AplikacijaPI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap laguna {
+            get {
+                object obj = ResourceManager.GetObject("laguna", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap poz2 {
             get {
                 object obj = ResourceManager.GetObject("poz2", resourceCulture);
